f525e3c1efc9fb984d1390365f8ece9509c592aa
dksalsgus <alsgus2ek@naver.com>
2021-12-01 21:17:45 +0900

    medialquery

--- lib/main.dart
+++ lib/main.dart
@@ -90,8 +90,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
       title: Text(
@@ -105,9 +105,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
     final txListWidget = Container(
-        height: (MediaQuery.of(context).size.height -
+        height: (mediaQuery.size.height -
                 appBar.preferredSize.height -
-                MediaQuery.of(context).padding.top) *
+                mediaQuery.padding.top) *
             0.7,
         child: TransactionList(transactions, _deleteTransaction));
     return Scaffold(
@@ -133,9 +133,9 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             if (!isLandscape)
               Container(
-                height: (MediaQuery.of(context).size.height -
+                height: (mediaQuery.size.height -
                         appBar.preferredSize.height -
-                        MediaQuery.of(context).padding.top) *
+                        mediaQuery.padding.top) *
                     0.3, // 미디어 쿼리로 전체 높이와 appbar 높이 상태바 높이로 계산
                 child: Chart(_recentTransactions),
               ),
@@ -143,9 +143,9 @@ class _MyHomePageState extends State<MyHomePage> {
             if (isLandscape)
               _showChart
                   ? Container(
-                      height: (MediaQuery.of(context).size.height -
+                height: (mediaQuery.size.height -
                               appBar.preferredSize.height -
-                              MediaQuery.of(context).padding.top) *
+                              mediaQuery.padding.top) *
                           0.7, // 미디어 쿼리로 전체 높이와 appbar 높이 상태바 높이로 계산
                       child: Chart(_recentTransactions),
                     )
